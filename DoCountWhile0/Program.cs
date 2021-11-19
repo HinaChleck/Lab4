6bce1372e794a5fe2c815f8396cbe7c552023a78
HinaChleck <hinachleck@gmail.com>
2021-11-20 00:31:03 +0500

    добавлено решение части 2

---
+++ DoCountWhile0/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoCountWhile0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательность положительных и отрицательных целых чисел:");
+            int y;
+            int iplus = 0;
+            int iminus = 0;
+           
+            do
+            {
+                y = Convert.ToInt32(Console.ReadLine());
+                if (y > 0) iplus++;
+                if (y < 0) iminus++;
+                
+            } while (y!=0);
+
+            Console.WriteLine("Количество положительных чисел - {0}", iplus);
+            Console.WriteLine("Количество отрицательных чисел - {0}", iminus);
+
+            Console.ReadKey();
+        }
+    }
+}
